cdd75a238339ee9c2d8723203cf88b7d431aecea
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2024-11-08 20:10:51 +0530

    contactusbyrohit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -21,8 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
+     
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         {children}
       </body>
     </html>
